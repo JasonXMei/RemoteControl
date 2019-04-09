918dc6f7b17fb27cd3d2902096630bb245ebe9ca
JasonMei <mei_bo999@sina.com>
2019-04-09 23:05:26 +0800

    adjust project structure & add monitor micro-service

---
+++ rc-monitor/src/main/java/com/jason/rcmonitor/RcMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.jason.rcmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcMonitorApplication.class, args);
+	}
+
+}
