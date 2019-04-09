db5af7b3ec6ab3b18d42b13c37fc9b08193752bc
JasonMei <mei_bo999@sina.com>
2019-04-09 20:59:38 +0800

    init project structure

---
+++ rc-client/src/main/java/com/jason/rcclient/RcClientApplication.java
@@ -0,0 +1,13 @@
+package com.jason.rcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcClientApplication.class, args);
+	}
+
+}
---
+++ rc-common/src/main/java/com/jason/rccommon/RcCommonApplication.java
@@ -0,0 +1,13 @@
+package com.jason.rccommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcCommonApplication.class, args);
+	}
+
+}
